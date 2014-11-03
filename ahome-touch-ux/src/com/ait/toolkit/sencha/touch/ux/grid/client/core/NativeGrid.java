0efa28769d4fc2586facab1bec76cab3eeb151e2
Ekambos <jazzmatadazz@gmail.com>
2014-11-04 00:03:25 +0100

    Revert "Mavenisation"

--- ahome-touch-ux/src/com/ait/toolkit/sencha/touch/ux/grid/client/core/NativeGrid.java
+++ ahome-touch-ux/src/com/ait/toolkit/sencha/touch/ux/grid/client/core/NativeGrid.java
@@ -19,12 +19,12 @@ import java.util.List;
 
 import com.ait.toolkit.sencha.shared.client.core.XType;
 import com.ait.toolkit.sencha.shared.client.data.Store;
-import com.ait.toolkit.sencha.touch.client.ui.ListView;
+import com.ait.toolkit.sencha.touch.client.ui.ListDataView;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArray;
 import com.google.gwt.core.client.JsArrayString;
 
-public class NativeGrid extends ListView {
+public class NativeGrid extends ListDataView {
 
 	protected NativeGrid(JavaScriptObject config) {
 		super(config);
